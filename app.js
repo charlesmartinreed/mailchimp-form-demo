0e090129f5f473b18eb8573be5400caedfad7033
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-24 22:01:14 -0500

    Create static folder for serving html.

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path = require('path');
 
 const app = express();
 
+// static folder for serving html...
+app.use(express.static(path.join(__dirname, 'public')));
+
 // start the server on port 500 if local
 const port = process.env.PORT || 5000;
 
